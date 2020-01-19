ab9b4a096eda9c9f9ab50260606212cee467430b
Nemanja <kazicnemanja1@gmail.com>
2020-01-19 13:31:32 +0100

    Add vue2-interact library for swiping gestures.

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -1,17 +1,28 @@
 <template>
-    <div class="card">
+    <Vue2InteractDraggable
+            :interact-out-of-sight-x-coordinate="500"
+            :interact-max-rotation="15"
+            interact-lock-y-axis="true"
+            :interact-x-threshold="100"
+            class="rounded-borders shadow-10 card">
         <img class="card-img-top" src="https://veza.iapi.ch/img/noimage.feb41bd2.png" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
-    </div>
+    </Vue2InteractDraggable>
 </template>
 
 <script>
+    import { Vue2InteractDraggable } from 'vue2-interact';
+
     export default {
-        name: "PropertyCard"
+        name: "PropertyCard",
+        components: {
+            Vue2InteractDraggable
+        }
     }
 </script>
 

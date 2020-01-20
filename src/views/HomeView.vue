77eb487629ab4a57424f4f531150c05a19f068f4
Nemanja <kazicnemanja1@gmail.com>
2020-01-20 13:26:43 +0100

    Add loading overlay while fetching data.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,42 @@
 <template>
     <section class="container position-relative">
+        <loading :active.sync="isLoading"
+                 :can-cancel="false"
+                 color="#27348b"
+                 :height="150"
+                 :width="200"
+                 :is-full-page="true"></loading>
         <filter-tab title="Filters" :expanded="false">
             <div>
                 <h1>Filters</h1>
             </div>
         </filter-tab>
-        <properties-stack></properties-stack>
+        <properties-stack v-if="!isLoading"></properties-stack>
     </section>
 </template>
 
 <script>
     import FilterTab from '../partials/FilterTab';
-    import PropertiesStack from '../components/PropertiesStack'
+    import PropertiesStack from '../components/PropertiesStack';
+    import Loading from 'vue-loading-overlay';
+    import 'vue-loading-overlay/dist/vue-loading.css';
 
     export default {
         name: 'SwipeableCards',
         components: {
             FilterTab,
+            Loading,
             PropertiesStack
         },
-        mounted(){
-            this.$store.dispatch('addSearchedProperties').then(() => console.log('Promise all finished.'));
+        data() {
+            return {
+                isLoading: true,
+            }
+        },
+        mounted() {
+            this.$store.dispatch('addSearchedProperties').then(() => {
+                this.isLoading = false;
+            });
         }
     }
 </script>
